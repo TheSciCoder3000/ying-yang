4688e71dcbbc4f13ba052db3741ef57c4b351dc0
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-09-09 18:03:09 +0800

    infinite marquee logo

--- components/InfiniteScroll.tsx
+++ components/InfiniteScroll.tsx
@@ -3,6 +3,7 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import React, { useRef } from "react";
+import Image from "./Image";
 
 gsap.registerPlugin(useGSAP);
 
@@ -32,7 +33,17 @@ const InfiniteScroll = () => {
       <div className="scroll-text" ref={containerRef}>
         {Array.from(Array(8).keys()).map((indx) => (
           <p className="text-span" key={indx}>
-            Because life&apos;s too short for boring designs ~
+            {/* Because life&apos;s too short for boring designs ~ */}
+            <Image
+              className="company-logo"
+              src="/companies/bmw.png"
+              alt="bmw"
+            />
+            <Image
+              className="company-logo"
+              src="/companies/yinyang-logo.png"
+              alt="bmw"
+            />
           </p>
         ))}
       </div>
