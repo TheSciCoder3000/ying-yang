9555d804cf44aa004bffe6627b14399ef2fbafd1
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-09-10 21:44:48 +0800

    reset components

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import ProjectProposal from "@/components/ProjectProposal";
 import Ratings from "@/components/Ratings";
 // import MetricsSection from "@/components/MetricsSection";
 import Services from "@/components/Services";
-import TestimonialV2 from "@/components/TestimonialV2";
 import VideoCarousel from "@/components/VideoCarousel";
 import WhatWeDo from "@/components/WhatWeDo";
 
@@ -19,8 +18,7 @@ export default function Home() {
     <div>
       <Hero />
       <InfiniteScroll />
-      <TestimonialV2 />
-      {/* <About />
+      <About />
       <Ratings />
       <Intro />
       <WhatWeDo />
@@ -30,7 +28,7 @@ export default function Home() {
       <VideoCarousel />
       <ProjectProposal />
       <Contact />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
