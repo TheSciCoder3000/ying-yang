abbdc49efa4ce9a7547a150b76db1d0fa8511f7d
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-09-11 00:23:25 +0800

    header tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import React from "react";
-import "@/styles/css/Header.css";
 import Image from "./Image";
 
 const Header = () => {
   return (
-    <header>
-      <Image src={"/yinyang-logo.png"} alt="logo" />
-      <div className="menu">• menu</div>
+    <header className="px-20 py-8 flex justify-between fixed top-0 left-0 w-screen z-999">
+      <Image className="w-fit h-9" src={"/yinyang-logo.png"} alt="logo" />
+      <div className="text-[#bc2637] font-black">• menu</div>
     </header>
   );
 };
