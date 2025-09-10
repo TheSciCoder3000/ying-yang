c430a8f1a34bd88ec4fe20c00a1de1fb2403788d
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-09-10 23:47:20 +0800

    contact tailwind

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "@/styles/css/Contact.css";
 import { CalendarDays, Check } from "lucide-react";
 import Image from "./Image";
 import ContactForm from "./ContactForm";
@@ -18,36 +17,40 @@ const Checklists = [
 
 const Contact = () => {
   return (
-    <div className="contact-cont">
-      <div className="left">
-        <div className="header">
-          <h1>Have a Project?</h1>
-          <h1>Let&apos;s talk</h1>
+    <div className="max-w-[70rem] my-30 mx-auto p-4 flex gap-20 ">
+      <div className="flex-1">
+        <div className="mb-8 space-y-5">
+          <h1 className="text-5xl font-[500]">Have a Project?</h1>
+          <h1 className="text-5xl font-[500]">Let&apos;s talk</h1>
         </div>
 
-        <div className="checklist-cont">
+        <div className="mb-[6rem]">
           {Checklists.map((item, indx) => (
             <ChecklistItem key={indx} text={item.label} />
           ))}
         </div>
 
         <div className="schedule-cont">
-          <h3>Schedule a call:</h3>
+          <h3 className="font-light mb-4">Schedule a call:</h3>
 
-          <div className="profile-cont">
-            <Image className="profile-img" src="/profile.jpg" alt="profile" />
-            <div className="profile-info">
-              <h2>Tanisha M Jain</h2>
-              <p>Founder & CEO</p>
+          <div className="flex gap-5 items-center">
+            <Image
+              className="profile-img aspect-square w-16 h-full"
+              src="/profile.jpg"
+              alt="profile"
+            />
+            <div>
+              <h2 className="text-xl mb-2">Tanisha M Jain</h2>
+              <p className="text-xs text-gray-400 font-light">Founder & CEO</p>
             </div>
-            <button>
+            <button className="cursor-pointer bg-[#402fed] rounded-full aspect-square w-10 h-10 text-white flex items-center justify-center">
               <CalendarDays size={15} />
             </button>
           </div>
         </div>
       </div>
 
-      <div className="right">
+      <div className="flex">
         <ContactForm />
       </div>
     </div>
@@ -59,7 +62,7 @@ interface ChecklistItemProps {
 }
 const ChecklistItem: React.FC<ChecklistItemProps> = ({ text }) => {
   return (
-    <div className="checklist-item">
+    <div className="flex gap-2 items-center mb-[2px] text-sm text-gray-400 font-light">
       <Check size={15} />
       {text}
     </div>
