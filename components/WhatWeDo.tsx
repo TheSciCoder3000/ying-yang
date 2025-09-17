286d399e9009828d9d17c07efb3a9b8d2e1df162
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-09-17 18:09:54 +0800

    what we do section observer

--- components/WhatWeDo.tsx
+++ components/WhatWeDo.tsx
@@ -5,7 +5,7 @@ import "@/styles/css/WhatWeDo.css";
 import Image from "./Image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/all";
+import { Observer, ScrollTrigger } from "gsap/all";
 
 const DataIndiv = [
   {
@@ -71,22 +71,141 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const WhatWeDo = () => {
   const containerRef = useRef<HTMLDivElement>(null);
-  useGSAP(
-    () => {
-      if (!containerRef.current) return;
 
-      gsap.from(".content-body", {
-        xPercent: (indx) => (indx % 2 === 0 ? 110 : -110),
-        scrollTrigger: {
-          trigger: ".content-cont",
-          start: "top 20",
-          pin: true,
-          scrub: true,
+  useGSAP(() => {
+    let allowScroll = true; // sometimes we want to ignore scroll-related stuff, like when an Observer-based section is transitioning.
+    const scrollTimeout = gsap
+      .delayedCall(0.5, () => (allowScroll = true))
+      .pause(); // controls how long we should wait after an Observer-based animation is initiated before we allow another scroll-related action
+    let currentIndex = 0;
+    const swipePanels = gsap.utils.toArray<HTMLDivElement>(".content-body");
+    const labels = gsap.utils.toArray<HTMLDivElement>(".for-who-item");
+    const isTouch = ScrollTrigger.isTouch;
+
+    if (isTouch === 1) {
+      ScrollTrigger.normalizeScroll(true);
+    }
+
+    // set z-index levels for the swipe panels
+    gsap.set(swipePanels, { zIndex: (i) => swipePanels.length - i });
+
+    interface MyObserver extends Observer {
+      _restoreScroll?: () => void;
+    }
+
+    // create an observer and disable it to start
+    const intentObserver = ScrollTrigger.observe({
+      type: "wheel,touch",
+      onUp: () => allowScroll && gotoPanel(currentIndex - 1, false),
+      onDown: () => allowScroll && gotoPanel(currentIndex + 1, true),
+      tolerance: 10,
+      preventDefault: true,
+      onEnable(self: MyObserver) {
+        allowScroll = false;
+        scrollTimeout.restart(true);
+        // when enabling, we should save the scroll position and freeze it. This fixes momentum-scroll on Macs, for example.
+        const savedScroll = self.scrollY();
+        self._restoreScroll = () => self.scrollY(savedScroll); // if the native scroll repositions, force it back to where it should be
+        document.addEventListener("scroll", self._restoreScroll, {
+          passive: false,
+        });
+      },
+      onDisable: (self: MyObserver) => {
+        if (self._restoreScroll)
+          document.removeEventListener("scroll", self._restoreScroll);
+      },
+    });
+    intentObserver.disable();
+
+    // handle the panel swipe animations
+    function gotoPanel(index: number, isScrollingDown: boolean) {
+      // return to normal scroll if we're at the end or back up to the start
+      if (
+        (index === swipePanels.length && isScrollingDown) ||
+        (index === -1 && !isScrollingDown)
+      ) {
+        intentObserver.disable(); // resume native scroll
+        return;
+      }
+      allowScroll = false;
+      scrollTimeout.restart(true);
+
+      const target = swipePanels[index];
+
+      const prevTarget = isScrollingDown
+        ? swipePanels[index - 1]
+        : swipePanels[index + 1];
+
+      const labelTarget = labels[index];
+
+      const prevLabel = isScrollingDown ? labels[index - 1] : labels[index + 1];
+
+      gsap.fromTo(
+        target,
+        {
+          y: isScrollingDown ? 100 : -100,
+          opacity: 0,
         },
+        {
+          y: 0,
+          opacity: 1,
+        }
+      );
+
+      gsap.to(prevTarget, {
+        y: isScrollingDown ? 100 : -100,
+        opacity: 0,
       });
-    },
-    { scope: containerRef }
-  );
+
+      gsap.fromTo(
+        labelTarget.querySelector("h1"),
+        {
+          background: "rgb(39, 39, 39)",
+          color: "gray",
+        },
+        {
+          background: "white",
+          color: "black",
+        }
+      );
+
+      gsap.fromTo(
+        prevLabel.querySelector("h1"),
+        {
+          background: "white",
+          color: "black",
+        },
+        {
+          background: "rgb(39, 39, 39)",
+          color: "gray",
+        }
+      );
+
+      currentIndex = index;
+    }
+
+    // pin swipe section and initiate observer
+    ScrollTrigger.create({
+      trigger: containerRef.current,
+      pin: true,
+      start: "top 10%",
+      onEnter: (self) => {
+        if (intentObserver.isEnabled) {
+          return;
+        } // in case the native scroll jumped past the end and then we force it back to where it should be.
+        self.scroll(self.start + 1); // jump to just one pixel past the start of this section so we can hold there.
+        intentObserver.enable(); // STOP native scrolling
+      },
+      onEnterBack: (self) => {
+        if (intentObserver.isEnabled) {
+          return;
+        } // in case the native scroll jumped backward past the start and then we force it back to where it should be.
+        self.scroll(self.end - 1); // jump to one pixel before the end of this section so we can hold there.
+        intentObserver.enable(); // STOP native scrolling
+      },
+    });
+  });
+
   return (
     <div ref={containerRef} className="we-do-cont">
       <div className="header">
@@ -100,9 +219,20 @@ const WhatWeDo = () => {
         </p>
       </div>
 
+      <div className="for-who-cont">
+        <div className="for-who-item">
+          <h1>For Individuals</h1>
+        </div>
+        <div className="for-who-item">
+          <h1>For Teams</h1>
+        </div>
+        <div className="for-who-item">
+          <h1>For Organizations</h1>
+        </div>
+      </div>
+
       <div className="content-cont">
         <div className="content-body">
-          <h3 className="content-header">For Individuals</h3>
           <div className="content-items">
             {DataIndiv.map((item, indx) => (
               <Item key={indx} {...item} />
@@ -110,7 +240,6 @@ const WhatWeDo = () => {
           </div>
         </div>
         <div className="content-body">
-          <h3 className="content-header">For Teams</h3>
           <div className="content-items">
             {DataTeams.map((item, indx) => (
               <Item key={indx} {...item} />
@@ -118,7 +247,6 @@ const WhatWeDo = () => {
           </div>
         </div>
         <div className="content-body">
-          <h3 className="content-header">For Organizations</h3>
           <div className="content-items">
             {DataOrg.map((item, indx) => (
               <Item key={indx} {...item} />
